2bbb45c09748a867160bfb8e445faddfd6e44802
Elisabeth Fainstein <elisabeth.fainstein@developpement-durable.gouv.fr>
2020-11-02 11:02:00 +0100

    first commit

---
+++ src/main/java/gouv/developpementdurable/capqualif/pocbackbeta/PocBackBetaApplication.java
@@ -0,0 +1,13 @@
+package gouv.developpementdurable.capqualif.pocbackbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBackBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBackBetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gouv/developpementdurable/capqualif/pocbackbeta/PocBackBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package gouv.developpementdurable.capqualif.pocbackbeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBackBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
