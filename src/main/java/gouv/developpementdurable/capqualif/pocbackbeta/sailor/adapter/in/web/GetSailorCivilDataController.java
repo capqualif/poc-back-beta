d9d1c8ded8e86770cdf497c86bc8c41771042caf
Elisabeth Fainstein <elisabeth.fainstein@developpement-durable.gouv.fr>
2020-11-03 20:29:56 +0100

    add cross-origin enabling

--- src/main/java/gouv/developpementdurable/capqualif/pocbackbeta/sailor/adapter/in/web/GetSailorCivilDataController.java
+++ src/main/java/gouv/developpementdurable/capqualif/pocbackbeta/sailor/adapter/in/web/GetSailorCivilDataController.java
@@ -3,20 +3,20 @@ package gouv.developpementdurable.capqualif.pocbackbeta.sailor.adapter.in.web;
 import gouv.developpementdurable.capqualif.pocbackbeta.sailor.application.port.in.GetSailorCivilDataUseCase;
 import gouv.developpementdurable.capqualif.pocbackbeta.sailor.domain.Sailor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sailors")
+@CrossOrigin(origins = "http://localhost:3000")
 public class GetSailorCivilDataController {
 
     @Autowired
     private GetSailorCivilDataUseCase getSailorCivilDataUseCase;
 
     @GetMapping("/{sailorNumber}")
+//    public String getSailorCivilData () {
+//        return "Hi ";
+//    }
     public Sailor getSailorCivilData (@PathVariable("sailorNumber") String id) {
         return getSailorCivilDataUseCase.getSailorCivilData(id);
     }
